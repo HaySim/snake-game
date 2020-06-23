fa61a77c5cde792259fbe3b0e0e4a0feb4be7c9c
HaySim <hayley_simmons@outlook.com>
2020-06-23 16:21:14 +0100

    implement direction functions

--- snake.py
+++ snake.py
@@ -1,11 +1,75 @@
 import turtle
+import time
+
+delay = 0.1
 
 # Set up the screen
 window = turtle.Screen()
 window.title("Snake Game")
 window.bgcolor("green")
 window.setup(width = 600, height = 600)
-window.tracer(0)
+window.tracer(0) # Turns off the screen updates
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+# Functions
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_left():
+    head.direction = "left"
+
+def go_right():
+    head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+window.listen()
+window.onkeypress(go_up, "Up")
+window.onkeypress(go_down, "Down")
+window.onkeypress(go_left, "Left")
+window.onkeypress(go_right, "Right")
+
+# Main game loop
+while True:
+    window.update()
+
+    move()
+
+    time.sleep(delay)
 
 window.mainloop()
-
